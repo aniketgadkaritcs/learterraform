0bb192270d0832bb27b26c639d3a3df8835d2c6e
aniketgadkaritcs <122387897+aniketgadkaritcs@users.noreply.github.com>
2024-02-11 15:36:51 -0500

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,1 +1,17 @@
-file 1
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "tfvpc"
+  }
+}
